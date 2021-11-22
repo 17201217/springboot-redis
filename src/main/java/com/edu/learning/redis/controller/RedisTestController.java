098366df3d1181d15aa7699cc1a6afd8b9472215
pjm <2557272503@qq.com>
2021-11-22 22:39:27 +0800

    springboot整合redis

---
+++ src/main/java/com/edu/learning/redis/controller/RedisTestController.java
@@ -0,0 +1,30 @@
+package com.edu.learning.redis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("/redis")
+public class RedisTestController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("/test")
+    public String testRedis() {
+        //设置值到redis
+        redisTemplate.opsForValue().set("name","lucy");
+        //从redis获取值
+        String name = (String)redisTemplate.opsForValue().get("name");
+        return name;
+    }
+}
